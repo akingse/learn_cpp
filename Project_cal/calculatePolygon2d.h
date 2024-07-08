5b49eaa11011b6e9c3ccab21c5c7c7e665f90d08
akingse <48311823+akingse@users.noreply.github.com>
2024-07-08 10:36:04 +0800

    函数inline

--- Project_cal/calculatePolygon2d.h
+++ Project_cal/calculatePolygon2d.h
@@ -90,7 +90,18 @@ namespace clash
 		return true;
 	}
 	
-	DLLEXPORT_CAL double calculatePolygonArea(const std::vector<Eigen::Vector2d>& polygon); //using Shoelace(Gauss) method
+	inline double calculatePolygonArea(const std::vector<Eigen::Vector2d>& polygon) //using Shoelace-Gauss method
+	{
+		double area = 0.0;
+		size_t n = polygon.size();
+		for (int i = 0; i < n; ++i)
+		{
+			int j = (i + 1) % n; //avoid over bound
+			area += (polygon[i][0] + polygon[j][0]) * (polygon[i][1] - polygon[j][1]); //wond
+			//area += polygon[i][0] * polygon[j][1] - polygon[i][1] * polygon[j][0]; //cross2d
+		}
+		return 0.5 * std::fabs(area);
+	}
 
 }
 #endif// CALCULATE_POLYGON2D_H
