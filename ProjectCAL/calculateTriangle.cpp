4c6678d58b40503cbf42d5c0a7f8a61e6cbe7371
akingse <48311823+akingse@users.noreply.github.com>
2025-09-20 20:41:50 +0800

    回退

--- ProjectCAL/calculateTriangle.cpp
+++ ProjectCAL/calculateTriangle.cpp
@@ -14,6 +14,11 @@ using namespace clash;
 //--------------------------------------------------------------------------------------------------
 
 #ifdef STATISTIC_DATA_COUNT
+std::atomic<size_t>
+count_isTwoTrisInter = 0, count_getTrisDistance = 0, count_isTrisBoundBoxInter = 0, count_isTrisAndBoxInter = 0,
+count_pointInTri = 0, count_err_degen_tri = 0,
+count_err_inputbox = 0, count_err_inter_dist = 0, count_err_repeat_tri = 0,
+count_err_tris_sepa = 0, count_err_tris_inter = 0;
 #endif //STATISTIC_DATA_COUNT
 #ifdef STATISTIC_DATA_TESTFOR
 #endif
