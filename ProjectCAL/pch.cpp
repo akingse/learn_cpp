65b27bf73960e44a8d8ad8aa03a5264848d33b91
akingse <48311823+akingse@users.noreply.github.com>
2025-09-24 13:57:12 +0800

    配置

--- ProjectCAL/pch.cpp
+++ ProjectCAL/pch.cpp
@@ -1,3 +1,26 @@
 #include "pch.h"
-// LibraryPath
-//$(SolutionDir)..\TPL\eigen-3.4.0;
+
+/*
+
+All project config
+
+General Default
+OutputDir
+$(SolutionDir)$(Platform)\$(Configuration)\
+TargetName
+$(ProjectName)
+动态库(.dll)
+
+VC++目录
+IncludePath
+$(SolutionDir)..\TPL\eigen-3.4.0;$(ProjectDir)..\;$(IncludePath)
+LibraryPath
+
+C/C++
+Preprocessor 预处理器宏
+_DEBUG;_CONSOLE;WIN32_LEAN_AND_MEAN;%(PreprocessorDefinitions)
+NDEBUG;_CONSOLE;%(PreprocessorDefinitions)
+
+Linker 链接器
+
+*/
