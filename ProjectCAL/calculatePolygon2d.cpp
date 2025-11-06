4bd79c17bf130b2a3630d655920638057400cbfb
akingse <48311823+akingse@users.noreply.github.com>
2025-11-06 20:42:02 +0800

    calculatePolygon2d整理

--- ProjectCAL/calculatePolygon2d.cpp
+++ ProjectCAL/calculatePolygon2d.cpp
@@ -1,3 +1,109 @@
 #include "pch.h"
+using namespace std;
+using namespace Eigen;
+using namespace eigen;
+using namespace clash;
 
+//for curvearray classify
+struct PathsPart
+{
+	int m_index = -2;//default init
+	double m_ymin = DBL_MAX;
+	//Eigen::Vector2d m_point;
+	Eigen::AlignedBox2d m_box2d;
+	std::vector<int> m_parity;
+	bool operator<(const PathsPart& rhs) const
+	{
+		return m_ymin < rhs.m_ymin;
+	}
+};
+
+static std::vector<PathsPart> _createPathsPartVct(const std::vector<std::vector<Eigen::Vector2d>>& profile)
+{
+	std::vector<PathsPart> pathsPartVct(profile.size());
+	for (int i = 0; i < profile.size(); ++i)
+	{
+		PathsPart& pathsPart = pathsPartVct[i];
+		pathsPart.m_index = i;
+		//pathsPart.m_point = profile[i][0];// profile must no empty
+		Eigen::AlignedBox2d box2d;
+		for (int j = 0; j < profile[i].size(); ++j)//(const auto& iter : profile[i])
+		{
+			const Eigen::Vector2d& point = profile[i][j];
+			box2d.extend(point);
+			if (point[1] < pathsPart.m_ymin)
+				pathsPart.m_ymin = point[1];
+		}
+		pathsPart.m_box2d = box2d;
+	}
+	std::sort(pathsPartVct.begin(), pathsPartVct.end());
+	return pathsPartVct;
+}
+
+//using plane sweep algorithm, using to convertPathsEigenToCurveArray
+std::vector<std::vector<int>> clash::getPathsClassify(const std::vector<std::vector<Eigen::Vector2d>>& profile)
+{
+	std::vector<PathsPart> pathsPartVct = _createPathsPartVct(profile);
+	//calculate profile whether inside
+	for (int i = 0; i < pathsPartVct.size(); ++i)
+	{
+		PathsPart& partI = pathsPartVct[i];
+		// ymin -> ymax
+		if (partI.m_index == -1) //not support inner nest inner
+			continue;
+		double ymax = partI.m_box2d.max()[1];
+		for (int j = i + 1; j < pathsPartVct.size(); ++j)
+		{
+			PathsPart& partJ = pathsPartVct[j];
+			if (partJ.m_index == -1)
+				continue;
+			if (ymax < partJ.m_ymin)
+				break;
+			//boundbox separate -> must separate
+			//boundbox intersect -> not always intersect
+			if (partI.m_box2d.intersects(partJ.m_box2d) &&
+				isPointInPolygon2D(profile[partJ.m_index][0], profile[partI.m_index]))
+			{
+				partI.m_parity.push_back(partJ.m_index);
+				partJ.m_index = -1; //means inner
+			}
+		}
+	}
+	// process index according sign
+	std::vector<std::vector<int>> unionVct;
+	for (int i = 0; i < pathsPartVct.size(); ++i)
+	{
+		PathsPart& partI = pathsPartVct[i];
+		if (partI.m_index == -1)
+			continue;
+		if (partI.m_parity.empty())
+		{
+			unionVct.push_back({ partI.m_index });
+			continue;
+		}
+		std::vector<int> parity = { partI.m_index };
+		parity.insert(parity.end(), partI.m_parity.begin(), partI.m_parity.end());
+		unionVct.push_back(parity);
+	}
+	return unionVct;
+}
+
+static bool isPolygonSelfIntersect_doubleLoop(const std::vector<Eigen::Vector2d>& polygon)
+{
+	if (polygon.size() <= 3)
+		return false;
+	int n = (int)polygon.size(); //even if n==3
+	for (int i = 0; i < n - 2; ++i) //skip adjacent
+	{
+		std::array<Eigen::Vector2d, 2> segmA = { polygon[i], polygon[i + 1] };
+		for (int j = i + 2; j < n; ++j)
+		{
+			//include i(0,1)-j(n-1,0)
+			std::array<Eigen::Vector2d, 2> segmB = { polygon[j], polygon[(j + 1) % n] };
+			if (clash::isTwoSegmentsIntersect(segmA, segmB))
+				return true;
+		}
+	}
+	return false;
+}
 
--- ProjectCAL/calculatePolygon2d.h
+++ ProjectCAL/calculatePolygon2d.h
@@ -126,38 +126,6 @@ namespace clash
 		return true;
 	}
 	
-	inline double calculatePolygonArea(const std::vector<Eigen::Vector2d>& polygon) //using Shoelace-Gauss method
-	{
-		double area = 0.0;
-		size_t n = polygon.size();
-		for (int i = 0; i < n; ++i)
-		{
-			int j = (i + 1) % n; //avoid index over bound
-			area += (polygon[i][0] + polygon[j][0]) * (polygon[i][1] - polygon[j][1]);
-			//area += polygon[i][0] * polygon[j][1] - polygon[i][1] * polygon[j][0]; //cross2d
-		}
-		return 0.5 * std::fabs(area);
-	}
-
-	inline bool isPolygonSelfIntersect_doubleLoop(const std::vector<Eigen::Vector2d>& polygon)
-	{
-		if (polygon.size() <= 3)
-			return false;
-        int n = (int)polygon.size(); //even if n==3
-		for (int i = 0; i < n - 2; ++i) //skip adjacent
-		{
-			std::array<Eigen::Vector2d, 2> segmA = { polygon[i], polygon[i + 1] };
-			for (int j = i + 2; j < n; ++j)
-			{
-				//include i(0,1)-j(n-1,0)
-				std::array<Eigen::Vector2d, 2> segmB = { polygon[j], polygon[(j + 1) % n] };
-				if (clash::isTwoSegmentsIntersect(segmA, segmB))
-					return true;
-			}
-		}
-		return false;
-	}
-
 	inline bool isPolygonSelfIntersect(const std::vector<Eigen::Vector2d>& polygon)
 	{
 		if (polygon.size() <= 3)
@@ -193,89 +161,43 @@ namespace clash
 		return false;
 	}
 
-	//for curvearray classify
-	struct PathsPart
+	inline double calculatePolygonArea(const std::vector<Eigen::Vector2d>& polygon) //using Shoelace-Gauss method
 	{
-		int m_index = -2;//default init
-		double m_ymin = DBL_MAX;
-		//Eigen::Vector2d m_point;
-		Eigen::AlignedBox2d m_box2d;
-		std::vector<int> m_parity;
-		bool operator<(const PathsPart& rhs) const
+		double area = 0.0;
+		size_t n = polygon.size();
+		for (int i = 0; i < n; ++i)
 		{
-			return m_ymin < rhs.m_ymin;
+			int j = (i + 1) % n; //avoid index over bound
+			area += (polygon[i][0] + polygon[j][0]) * (polygon[i][1] - polygon[j][1]);
+			//area += polygon[i][0] * polygon[j][1] - polygon[i][1] * polygon[j][0]; //cross2d
 		}
-	};
-
-	inline std::vector<PathsPart> _createPathsPartVct(const std::vector<std::vector<Eigen::Vector2d>>& profile)
-	{
-		std::vector<PathsPart> pathsPartVct(profile.size());
-		for (int i = 0; i < profile.size(); ++i)
-		{
-			PathsPart& pathsPart = pathsPartVct[i];
-			pathsPart.m_index = i;
-			//pathsPart.m_point = profile[i][0];// profile must no empty
-			Eigen::AlignedBox2d box2d;
-			for (int j = 0; j < profile[i].size(); ++j)//(const auto& iter : profile[i])
-			{
-				const Eigen::Vector2d& point = profile[i][j];
-				box2d.extend(point);
-				if (point[1] < pathsPart.m_ymin)
-					pathsPart.m_ymin = point[1];
-			}
-			pathsPart.m_box2d = box2d;
-		}
-		std::sort(pathsPartVct.begin(), pathsPartVct.end());
-		return pathsPartVct;
+		return 0.5 * std::fabs(area);
 	}
 
-	//using plane sweep algorithm, using to convertPathsEigenToCurveArray
-	inline std::vector<std::vector<int>> getPathsClassify(const std::vector<std::vector<Eigen::Vector2d>>& profile)
+	inline Eigen::Vector2d computeCentroid(const std::vector<Eigen::Vector2d>& polygon)
 	{
-		std::vector<PathsPart> pathsPartVct = _createPathsPartVct(profile);
-		//calculate profile whether inside
-		for (int i = 0; i < pathsPartVct.size(); ++i)
+		double area = 0.0;
+		Eigen::Vector2d centroid(0.0, 0.0);
+		int n = (int)polygon.size();
+		if (n < 3) 
 		{
-			PathsPart& partI = pathsPartVct[i];
-			// ymin -> ymax
-			if (partI.m_index == -1) //not support inner nest inner
-				continue;
-			double ymax = partI.m_box2d.max()[1];
-			for (int j = i + 1; j < pathsPartVct.size(); ++j)
-			{
-				PathsPart& partJ = pathsPartVct[j];
-				if (partJ.m_index == -1)
-					continue;
-				if (ymax < partJ.m_ymin)
-					break;
-				//boundbox separate -> must separate
-                //boundbox intersect -> not always intersect
-				if (partI.m_box2d.intersects(partJ.m_box2d) &&
-					isPointInPolygon2D(profile[partJ.m_index][0], profile[partI.m_index]))
-				{
-					partI.m_parity.push_back(partJ.m_index);
-					partJ.m_index = -1; //means inner
-				}
-			}
+			throw std::invalid_argument("Polygon must have at least 3 vertices.");
+			return Eigen::Vector2d(std::nan("0"), 0);
 		}
-		// process index according sign
-		std::vector<std::vector<int>> unionVct;
-		for (int i = 0; i < pathsPartVct.size(); ++i)
+		for (int i = 0; i < n; ++i) 
 		{
-			PathsPart& partI = pathsPartVct[i];
-			if (partI.m_index == -1)
-				continue;
-			if (partI.m_parity.empty())
-			{
-				unionVct.push_back({ partI.m_index });
-				continue;
-			}
-			std::vector<int> parity = { partI.m_index };
-			parity.insert(parity.end(), partI.m_parity.begin(), partI.m_parity.end());
-			unionVct.push_back(parity);
+			int j = (i + 1) % n; // Next vertex index
+			double crossProduct = polygon[i].x() * polygon[j].y() - polygon[j].x() * polygon[i].y();
+			area += crossProduct;
+			centroid += (polygon[i] + polygon[j]) * crossProduct; // Accumulate weighted centroid
 		}
-		return unionVct;
+		area *= 0.5;
+		centroid /= (6.0 * area); // Final centroid calculation
+		return centroid;
 	}
+
+	//for FillProfile
+	DLLEXPORT_CAL std::vector<std::vector<int>> getPathsClassify(const std::vector<std::vector<Eigen::Vector2d>>& profile);
 
 }
 #endif// CALCULATE_POLYGON2D_H
