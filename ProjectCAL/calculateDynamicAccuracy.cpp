94116e08ee41f86ddc4c873a4bf1f7c825e66db6
akingse <48311823+akingse@users.noreply.github.com>
2026-04-09 16:49:18 +0800

    命名空间

--- ProjectCAL/calculateDynamicAccuracy.cpp
+++ ProjectCAL/calculateDynamicAccuracy.cpp
@@ -4,6 +4,6 @@ using namespace Eigen;
 using namespace accura;
 
 //Precision
-double Precision::sm_toleAngle = 1e-12;
-double Precision::sm_toleConfusion = 1e-7;
+double accura::Precision::sm_toleAngle = 1e-12;
+double accura::Precision::sm_toleConfusion = 1e-7;
 
